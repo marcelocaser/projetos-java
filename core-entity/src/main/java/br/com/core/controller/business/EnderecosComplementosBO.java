265cdffa0a584483fac916f95dae5553f543cdea
marcelocaser <marcelocaser@gmail.com>
2016-03-02 13:11:42 -0300

    alteração de entidades

---
+++ core-entity/src/main/java/br/com/core/controller/business/EnderecosComplementosBO.java
@@ -0,0 +1,51 @@
+package br.com.core.controller.business;
+
+import br.com.core.entity.EnderecosComplementosTO;
+import br.com.core.persistence.interfaces.EnderecosComplementos;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author marce
+ */
+@Component
+public class EnderecosComplementosBO {
+    
+     @Autowired
+     EnderecosComplementos persistencia;
+
+    public void alterar(EnderecosComplementosTO complementosTO) {
+        this.persistencia.alterar(complementosTO);
+    }
+    
+    private void antesDeAlterar(EnderecosComplementosTO complementosTO) {
+        complementosTO.setAlteracao(new Date());
+    }
+
+    private void antesDeExcluir(EnderecosComplementosTO complementosTO) {
+        complementosTO.setExclusao(new Date());
+    }
+
+    public void excluir(EnderecosComplementosTO EnderecosComplementosTO) {
+        antesDeExcluir(EnderecosComplementosTO);
+        this.persistencia.excluir(EnderecosComplementosTO);
+
+    }
+
+    public void incluir(EnderecosComplementosTO EnderecosComplementosTO) {
+        antesDeAlterar(EnderecosComplementosTO);
+        this.persistencia.incluir(EnderecosComplementosTO);
+    }
+
+    public EnderecosComplementosTO consultar(EnderecosComplementosTO EnderecosComplementosTO) {
+        return this.persistencia.consultar(EnderecosComplementosTO);
+    }
+
+    public List<EnderecosComplementosTO> listar(EnderecosComplementosTO EnderecosComplementosTO) {
+        return this.persistencia.listar(EnderecosComplementosTO);
+    }
+    
+}
